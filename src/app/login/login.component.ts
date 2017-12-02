10b0e6aab8a6b28701d4d51fa44715f3369c8e7e
Neha Palekar <neha.palekar@dezinersstudio.com>
2017-12-02 20:46:51 +0530

    layout fixes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  private loginText: String = '';
+  public loginText: String = '';
 
   constructor() { }
 
